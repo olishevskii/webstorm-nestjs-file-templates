02c36d2bfa506f9f1ed813bdf1341a4e943dfb5f
Olishevsky Alex <olishevskyalex@gmail.com>
2023-07-09 19:50:33 +0400

    Improve service template

---
+++ .idea/fileTemplates/Nest Service༏ext༏.service.ts
@@ -0,0 +1,9 @@
+#set( $className = ${StringUtils.removeAndHump(${NAME}, "-")} )
+import { Injectable, Logger } from "@nestjs/common";
+
+@Injectable()
+export class ${className}Service {
+  private readonly logger = new Logger(${className}Service.name)  
+    
+  constructor() {}
+}
